aa55712fc894e527f07b3beaef100d3b3981ed35
Patryk Czechowski <patryk.czechowski@student.pk.edu.pl>
2024-08-12 20:21:55 +0200

    init-footer

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, MatIconModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'angular-app';
