6a72b25e951102f0f5e78e3d3121110aa1a76bb6
Patryk Czechowski <patryk.czechowski@student.pk.edu.pl>
2024-08-12 22:28:19 +0200

    save

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MainContentComponent } from './main-content/main-content.component';
+import { TopNavigationBarComponent } from './top-navigation-bar/top-navigation-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, MatIconModule],
+  imports: [RouterOutlet, FooterComponent, MatIconModule, MainContentComponent, TopNavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
